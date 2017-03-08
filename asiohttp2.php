9532114bd4de3c802c4b812b564f4779a0f63624
lihaifeng <lihaifeng@ushow.media>
2017-03-08 00:20:06 -0800

    update test script

--- asiohttp2.php
+++ asiohttp2.php
@@ -10,12 +10,4 @@ echo "Functions available in the test extension:$br\n";
 foreach($functions as $func) {
     echo $func."$br\n";
 }
-echo "$br\n";
-$function = 'confirm_' . $module . '_compiled';
-if (extension_loaded($module)) {
-	$str = $function($module);
-} else {
-	$str = "Module $module is not compiled into PHP";
-}
-echo "$str\n";
 ?>
